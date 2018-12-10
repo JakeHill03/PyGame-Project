8590bea58c2a617ed53777d9e775500c1b857d91
srtodd97 <45235037+srtodd97@users.noreply.github.com>
2018-12-10 12:31:11 +0000

    comments

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 28 19:58:09 2018
 
-NEXT HOP! SETTINGS
-@author: srtod
-"""
-
+#define variable for the colours
 white = (255, 255, 255)
 blue = (0, 0, 200)
 light_blue = (153, 204, 255)
+
+#set the framerate
 FPS = 60
 
+#sets file destination for highscore
 hs_file = "highscore.txt"
+
+#sets fontname
 Font_Name="scoreboard"
--- sounds.py
+++ sounds.py
@@ -8,14 +8,18 @@ import pygame
 from os import path
 
 pygame.init()
+
+#defines path to directories for sound files and image files
 image_dir = path.join(path.dirname(__file__), 'images') #Add path to use the images folder, so files can be referenced
 music_dir = path.join(path.dirname(__file__), 'music') #Add path to use sound folder 
 
-#Load in sounds
+#Loads in sounds
 pygame.mixer.music.load(path.join(music_dir, "background_music.wav"))
 falling_noise = pygame.mixer.Sound(path.join(music_dir, "falling_sound.wav"))
 jumping_noise = pygame.mixer.Sound(path.join(music_dir, "jump_sound.wav"))
 powerup_noise = pygame.mixer.Sound(path.join(music_dir, "powerup.wav"))
+
+#initilaises, sets volume and loops background music
 pygame.mixer.pre_init(44100,16,2,4096) #initialises the pygame mixer module for loading and playing sound files and music
 pygame.mixer.music.set_volume(0.5) #Set music volume
 pygame.mixer.music.play(-1) #Play music
